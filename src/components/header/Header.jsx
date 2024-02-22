abd7c83cdce481a829babe59e347562eb404fa48
Smita Priya <smitabhushan58@gmail.com>
2024-02-23 02:16:02 +0530

    error fixed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -79,7 +79,8 @@ const Header = () => {
         <header className={`header ${mobileMenu ? "mobileView" : ""} ${show}`}>
             <ContentWrapper>
                <div className="logo" onClick={()=>  navigate("/")}>
-               <img src={logo} alt=""/> 
+               {/*<img src={logo} alt=""/>*/} 
+               <h2>CHILLAX</h2>
                 </div> 
                <ul className="menuItems">
                 <li className="menuItem" onClick={()=>navigationHandler("movie")}>Movies</li>
